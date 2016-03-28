fe34ee4db7e6e0ce8a922c41062189984208fea5
Jueast Wang <jueastwang@outlook.com>
2016-03-28 22:38:52 +0800

    add init.c

---
+++ init/init.c
@@ -0,0 +1,45 @@
+#include <printf.h>
+
+void arm_init(){
+	printf("init.c:\tarm_init() is called\n");
+	
+	panic("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+	while(1);
+	panic("init.c:\t end of arm_init() reached!");
+}
+
+void bcopy(const void *src, void *dst, size_t len){
+	void *max;
+
+	max = dst + len;
+
+	while (dst + 3 < max) {
+		*(int *)dst = *(int *) src;
+		dst += 4;
+		src += 4;
+	}
+	
+	while(dst < max) {
+		
+		*(char *)dst = *(char *)src;
+		dst += 1;
+		src += 1;
+	}
+}
+
+void bzero(void *b, size_t len){
+	void *max;
+        max = b + len;
+
+        while (b + 3 < max)
+        {
+                *(int *)b = 0;
+                b+=4;
+        }
+        
+        while (b < max)
+        {
+                *(char *)b++ = 0;
+        }
+
+}
